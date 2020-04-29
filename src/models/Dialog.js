184195c5b46d8739f5fc6f8d953eb1492f6e22a3
isabelleMello <isabelle.melo@dcx.ufpb.br>
2020-04-28 23:45:05 -0300

    Dialogue between users

---
+++ src/models/Dialog.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const DialogSchema = new Schema(
+  {
+    speech: {
+      type: String,
+      required: true,
+    },
+
+    answer: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    approvals: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    disapprovals: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Dialog", DialogSchema);
