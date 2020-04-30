245879587e7924c532a09018c931e84f9c86d98b
barbosamaatheus <maatheusb96@gmail.com>
2020-04-30 04:22:03 -0300

    refactor(#9): mover mongoose para server.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const routes = require("./routes");
-const config = require("./utils/config");
 
 const app = express();
-
-mongoose.connect(config.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
 app.use(express.json());
 app.use(routes);
 
-module.exports = { app, mongoose };
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
-const { app } = require("./app");
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const config = require("./utils/config");
 
 const PORT = process.env.PORT || 3000;
 
+mongoose.connect(config.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+console.log("Conectado: ", config.MONGODB_URI);
 app.listen(PORT);
