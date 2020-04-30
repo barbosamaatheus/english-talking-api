fe2058252e1bc73be1cda9e870a7275626c31bb1
barbosamaatheus <maatheusb96@gmail.com>
2020-04-30 15:31:58 -0300

    refactor(#17): utilizar userid no create dialog

--- src/Services/CreateDialogService.js
+++ src/Services/CreateDialogService.js
@@ -1,14 +1,12 @@
 const Dialog = require("../models/Dialog");
 
 module.exports = async (req, res) => {
-  const { speech, answer } = req.body;
-
-  const response = await Dialog.create({ speech, answer })
+  const response = await Dialog.create({ ...req.body, user: req.userId })
     .then((item) => res.status(201).json(item))
     .catch((error) =>
       error.name === "ValidationError"
-        ? res.status(400).json(error.errors)
-        : res.status(500).json(error)
+        ? res.status(400).json({ error: error.message })
+        : res.status(500).json({ error: "Dialog creation failed" })
     );
 
   return response;
