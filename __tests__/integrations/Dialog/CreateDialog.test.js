8be7c305cc154afd988219df02beb98bc17609c2
isabelleMello <isabelle.melo@dcx.ufpb.br>
2020-05-12 14:29:13 -0300

    test(#20): " Add validation test to a dialog"

---
+++ __tests__/integrations/Dialog/CreateDialog.test.js
@@ -0,0 +1,97 @@
+const supertest = require("supertest");
+const faker = require("faker");
+const app = require("../../../src/app");
+
+const request = supertest(app);
+
+let authorization;
+beforeAll(async () => {
+  const response = await request.post("/v1/register").send({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  });
+
+  authorization = `Bearer ${response.body.token}`;
+});
+
+describe("Authentication POST /dialog", () => {
+  const speech = faker.lorem.sentence();
+  const answer = faker.lorem.sentence();
+
+  it("No token provider", async () => {
+    const response = await request.post("/v1/dialog").send({ speech, answer });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.error).toBe("No token provider");
+  });
+
+  it("Token error", async () => {
+    const response = await request
+      .post("/v1/dialog")
+      .set("Authorization", "Outher Token Bearer")
+      .send({ speech, answer });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.error).toBe("Token error");
+  });
+
+  it("Token malformatted", async () => {
+    const response = await request
+      .post("/v1/dialog")
+      .set("Authorization", "Beareer token")
+      .send({ speech, answer });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.error).toBe("Token malformatted");
+  });
+
+  it("Token invalid", async () => {
+    const response = await request
+      .post("/v1/dialog")
+      .set("Authorization", "Bearer token")
+      .send({ speech, answer });
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body.error).toBe("Token invalid");
+  });
+});
+
+describe("Create Dialog", () => {
+  it("Check dialogue creation with all fields correctly fille d.", async () => {
+    const speech = faker.lorem.sentence();
+    const answer = faker.lorem.sentence();
+
+    const response = await request
+      .post("/v1/dialog")
+      .set("Authorization", authorization)
+      .send({ speech, answer });
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.speech).toBe(speech);
+    expect(response.body.answer).toBe(answer);
+    expect(response.body.status).toBe("analyzing");
+  });
+
+  it("Check dialogue creation without sending the field speech.", async () => {
+    const speech = faker.lorem.sentence();
+
+    const response = await request
+      .post("/v1/dialog")
+      .set("Authorization", authorization)
+      .send({ speech });
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  it("Check dialogue creation without sending answer field.", async () => {
+    const answer = faker.lorem.sentence();
+
+    const response = await request
+      .post("/v1/dialog")
+      .set("Authorization", authorization)
+      .send({ answer });
+
+    expect(response.statusCode).toBe(400);
+  });
+});
