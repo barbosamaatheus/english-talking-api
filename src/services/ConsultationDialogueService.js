f73b898ab454ab0806292d7e69caf25e87057755
isabelleMello <isabelle.melo@dcx.ufpb.br>
2020-05-15 23:42:10 -0300

    fix(#20): return the count correctly

--- src/services/ConsultationDialogueService.js
+++ src/services/ConsultationDialogueService.js
@@ -8,6 +8,7 @@ function parserPageArgs(property, value) {
   return passed;
 }
 
+// eslint-disable-next-line consistent-return
 module.exports = async (req, res) => {
   const { limit, page } = req.headers;
 
@@ -15,27 +16,30 @@ module.exports = async (req, res) => {
     limit: parserPageArgs("limit", limit),
     page: parserPageArgs("page", page),
   };
-  const dialogues = await Dialog.find(req.query, null, {
-    limit: options.limit,
-    skip: (options.page - 1) * options.limit,
-  }).catch((error) =>
-    error.name === "ValidationError"
-      ? res.status(400).json({ error: error.message })
-      : res.status(500).json({ error: "Dialog consultation failed" })
-  );
-  if (dialogues === null || dialogues.length === 0) {
-    return res.status(404).send();
+  try {
+    const dialogues = await Dialog.find(req.query, null, {
+      limit: options.limit,
+      skip: (options.page - 1) * options.limit,
+    });
+
+    const count = await Dialog.countDocuments(req.query);
+    res.header("X-Total-Count", count);
+
+    if (!count) return res.status(404).send();
+
+    const response = dialogues.map((element) => {
+      return {
+        // eslint-disable-next-line dot-notation
+        ...element["_doc"],
+        approval_rate: element.approval_rate,
+      };
+    });
+    return res.status(200).json(response);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).json({ error: err.message });
+    } else {
+      res.status(500).json({ error: "Dialog consultation failed" });
+    }
   }
-  const response = dialogues.map((element) => {
-    return {
-      // eslint-disable-next-line dot-notation
-      ...element["_doc"],
-      approval_rate: element.approval_rate,
-    };
-  });
-
-  const count = await Dialog.countDocuments(req.query);
-  res.header("X-Total-Count", count);
-
-  return res.status(200).json(response);
 };
