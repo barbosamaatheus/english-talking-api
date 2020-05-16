6ef8b3f735c1f90106d6fc4c7839a849a7e99643
isabelleMello <isabelle.melo@dcx.ufpb.br>
2020-05-16 17:40:26 -0300

    fix(#20): add a change in function

--- src/utils/calculatesApprovalRate.js
+++ src/utils/calculatesApprovalRate.js
@@ -1,8 +1,6 @@
-const DialogSchema = require("../models/Dialog");
-
-DialogSchema.virtual("approval_rate").get(function CalculatesApprovalRate() {
+exports.default = function CalculatesApprovalRate() {
   const total = this.approvals.length + this.disapprovals.length;
 
   if (!this.approvals.length) return 0;
   return parseInt((this.approvals.length / total) * 100, 10);
-});
+};
