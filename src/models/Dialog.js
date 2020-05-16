aaaddd5827e78592b2cb48f0b761874cb64d67d0
isabelleMello <isabelle.melo@dcx.ufpb.br>
2020-05-15 23:38:18 -0300

    chore(#20): extracted a function

--- src/models/Dialog.js
+++ src/models/Dialog.js
@@ -38,11 +38,4 @@ const DialogSchema = new Schema(
   }
 );
 
-DialogSchema.virtual("approval_rate").get(function CalculatesApprovalRate() {
-  const total = this.approvals.length + this.disapprovals.length;
-
-  if (!this.approvals.length) return 0;
-  return parseInt((this.approvals.length / total) * 100, 10);
-});
-
 module.exports = model("Dialog", DialogSchema);
