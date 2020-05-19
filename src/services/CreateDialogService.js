d88eea8c8f5c49f52242874af8c6be844f28250e
LaksCastro <lakhsmycastro336@gmail.com>
2020-05-19 11:54:28 -0300

    fix: change then() and catch() to async, await syntax

--- src/services/CreateDialogService.js
+++ src/services/CreateDialogService.js
@@ -1,13 +1,16 @@
 const Dialog = require("../models/Dialog");
 
 module.exports = async (req, res) => {
-  const response = await Dialog.create({ ...req.body, user: req.userId })
-    .then((item) => res.status(201).json(item))
-    .catch((error) =>
-      error.name === "ValidationError"
-        ? res.status(400).json({ error: error.message })
-        : res.status(500).json({ error: "Dialog creation failed" })
-    );
+  try {
+    const dialog = await Dialog.create({
+      ...req.body,
+      user: req.userId,
+    });
 
-  return response;
+    return res.status(201).json(dialog);
+  } catch (error) {
+    return error.name === "ValidationError"
+      ? res.status(400).json({ error: error.message })
+      : res.status(500).json({ error: "Dialog creation failed" });
+  }
 };
