0af6eef17b84e13f0dce5e9f7ffcb4aa302aa318
isabelleMello <isabelle.melo@dcx.ufpb.br>
2020-05-22 23:45:55 -0300

    feat(#23): add reject method

--- src/controllers/RatingsController.js
+++ src/controllers/RatingsController.js
@@ -1,8 +1,13 @@
 const ApprovalDialogService = require("../services/ApprovalDialogService");
+const RejectDialogService = require("../services/RejectDialogService");
 
 module.exports = {
   async approval(req, res) {
     const response = await ApprovalDialogService(req, res);
+    return response;
+  },
+  async reject(req, res) {
+    const response = await RejectDialogService(req, res);
     return response;
   },
 };
