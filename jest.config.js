2af0efc90c6ac00a229148e0b13b94e51bc054a3
William Porto <powilliam19@gmail.com>
2020-05-27 03:27:39 -0400

    fix(#33): collect coverage just from ts files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   clearMocks: true,
   collectCoverage: false,
-  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  collectCoverageFrom: ["src/**/*.{ts}"],
   coverageDirectory: "__tests__/coverage",
   setupFilesAfterEnv: ["./__tests__/setup.ts"],
   testEnvironment: "node",
