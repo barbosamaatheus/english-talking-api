2ce3cb0cded37ba318c8ee90b1e1428a9d979fbe
Barbosa <mbo2@cin.ufpe.br>
2020-12-09 19:01:00 -0300

    chore: reconfigure to use database url

---
+++ ormconfig.ts
@@ -0,0 +1,10 @@
+export default {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  migrationsRun: true,
+  entities: ["./src/models/*.ts"],
+  migrations: ["./src/database/migrations/*.ts"],
+  cli: {
+    migrationsDir: "./src/database/migrations",
+  },
+};
