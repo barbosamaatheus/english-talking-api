95cbc6213e806361a15794fc3b987e59d5e1bab2
Barbosa <mbo2@cin.ufpe.br>
2020-12-10 13:01:07 -0300

    chore: update ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,39 @@
+const production = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  schema: "public",
+  entities: ["dist/src/models/*.js"],
+  migrations: ["dist/src/database/migrations/*.js"],
+  migrationsRun: true,
+  logging: false,
+};
+
+const development = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  entities: ["src/models/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
+  migrationsRun: true,
+};
+
+const tests = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  entities: ["src/models/*.ts"],
+  migrations: ["src/database/migrations/*.ts"],
+  migrationsRun: true,
+};
+
+const base = {
+  production,
+  development,
+  tests,
+};
+
+module.exports = {
+  ...base[process.env.NODE_ENV || "development"],
+  cli: {
+    migrationsDir: "src/database/migrations",
+    entitiesDir: "src/models",
+  },
+};
