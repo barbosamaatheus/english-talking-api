f38ce3e590688cca29605d491c4539845ea0f8d5
Barbosa <mbo2@cin.ufpe.br>
2020-12-10 10:08:38 -0300

    chore: update ormconfig

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,8 +2,8 @@ module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
   migrationsRun: true,
-  entities: ["./src/models/*.ts"],
-  migrations: ["./src/database/migrations/*.ts"],
+  entities: ["./src/models/*.[ts, js]"],
+  migrations: ["./src/database/migrations/*.[ts, js]"],
   cli: {
     migrationsDir: "./src/database/migrations",
   },
